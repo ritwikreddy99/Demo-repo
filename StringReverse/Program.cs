ffbf88a43c8dbe44fb671e27dafab290ed394036
ritwi <ritwi@DESKTOP-E8E6HT2>
2024-08-21 14:00:11 -0400

    Add project files.

---
+++ StringReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string to reverse:");
+            string input = Console.ReadLine();
+
+            string reversedString = ReverseString(input);
+
+            Console.WriteLine("Reversed string: " + reversedString);
+            Console.ReadLine();
+        }
+
+       public static string ReverseString(string str)
+        {
+            string reversed = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+
+            return reversed;
+        }
+    }
+}
+    
+
